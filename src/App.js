779b80c4de8507c3425fedc74b605ce0a37c7172
Caio Porto <caioportoramos@gmail.com>
2022-02-15 17:38:57 -0300

    APROVEM ESTE COMMIT

--- src/App.js
+++ src/App.js
@@ -4,6 +4,16 @@ import Nav from "./components/Nav";
 import Home from "./pages/Home";
 import ProductCards from "./components/ProductCards";
 
+const StyledHeader = styled.header`
+  background-color: #7e5185;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  font-family: "Skranji", cursive;
+  color: white;
+  font-size: 2rem;
+`;
+
 export default class App extends React.Component {
   addCart = () => {
     alert("O produto foi adicionado ao carrinho");
@@ -11,9 +21,9 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        <header>
+        <StyledHeader>
           <Nav />
-        </header>
+        </StyledHeader>
         <main>
           <Home />
         </main>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import styled from "styled-components";
 import ProductCards from "../components/ProductCards";
-
+import imagem1 from "../assets/pedra1.jpg";
+import imagem2 from "../assets/pedra2.jpg";
+import imagem3 from "../assets/pedra3.jpg";
+import imagem4 from "../assets/pedra4.jpg";
 
 const arrayDePedra = [
   {
@@ -41,10 +44,22 @@ export default class Home extends React.Component {
           <input>{/* Barra de Pesquisa */}</input>
         </div>
         <section>
-          <ProductCards />
-          <ProductCards />
-          <ProductCards />
-          <ProductCards />
+          <ProductCards
+            imagem="https://s1.static.brasilescola.uol.com.br/be/conteudo/images/imagem-pedras-crack-4f6c906404789.jpg"
+            produto="Pedra Mística de Crack"
+          />
+          <ProductCards
+            imagem="https://pbs.twimg.com/tweet_video_thumb/DZvmrfmXcAUoHGb.jpg"
+            produto="Pedra Mística de Vênus"
+          />
+          <ProductCards
+            imagem="https://anatomich.com/wp-content/uploads/2019/09/significado-da-pedra-topazio-ana-tomich-joias-personalizadas.jpg"
+            produto="Pedra Mística de Jovem Místico"
+          />
+          <ProductCards
+            imagem="https://chocairmedicos.com.br/wp-content/uploads/2021/07/pedra-no-rim-o-que-voce-precisa-saber-chocair-medicos-dcd.jpg"
+            produto="Pedra Mística de Rim"
+          />
         </section>
       </div>
     );
