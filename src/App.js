bd69a32946951c4d707f785d0d516dfbff16f4a3
CamilaCoradi <camila_coradi@hotmail.com>
2022-02-16 17:39:27 -0300

    commit-inicial

--- src/App.js
+++ src/App.js
@@ -1,32 +1,14 @@
 import React from "react";
 import styled from "styled-components";
-import Nav from "./components/Nav";
-import Home from "./pages/Home";
-import ProductCards from "./components/ProductCards";
 
-const StyledHeader = styled.header`
-  background-color: #7e5185;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-evenly;
-  font-family: "Skranji", cursive;
-  color: white;
-  font-size: 2rem;
-`;
 
 export default class App extends React.Component {
-  addCart = () => {
-    alert("O produto foi adicionado ao carrinho");
-  };
+ 
   render() {
     return (
       <div>
-        <StyledHeader>
-          <Nav />
-        </StyledHeader>
-        <main>
-          <Home />
-        </main>
+       
+        
       </div>
     );
   }
