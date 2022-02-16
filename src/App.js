752d6b1daf641dfe2f61f279151ebbc27c3fb83f
CamilaCoradi <camila_coradi@hotmail.com>
2022-02-16 17:23:56 -0300

    catarina zero

--- src/App.js
+++ src/App.js
@@ -1,31 +1,11 @@
 import React from "react";
 import styled from "styled-components";
-import Nav from "./components/Nav";
-import Home from "./pages/Home";
-import ProductCards from "./components/ProductCards";
-import Carrinho from "./pages/Carrinho";
-
-const StyledHeader = styled.header`
-  background-color: #7e5185;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-evenly;
-  font-family: "Skranji", cursive;
-  color: white;
-  font-size: 2rem;
-`;
 
 export default class App extends React.Component {
     render() {    
     return (
       <div>
-        <StyledHeader>
-          <Nav />
-        </StyledHeader>
-        <main>
-          <Home />
-          
-        </main>
+        
       </div>
     );
   }
