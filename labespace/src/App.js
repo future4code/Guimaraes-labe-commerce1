98b860eb3c2ae85b9f3a6349963668da567cfcd0
Laine Moura <lainermoura@gmail.com>
2022-02-17 13:06:14 -0300

    renderiza produtos com map e estiliza Card e seus items

--- labespace/src/App.js
+++ labespace/src/App.js
@@ -1,37 +1,42 @@
 import React from "react";
-import Produtos from "./Components/Produtos";
-import CardProdutos from "./Components/CardProdutos";
+import { Produtos } from "./Components/Produtos";
+import styled from 'styled-components'
+
+
+
+
 
 const products = [
   {
     id: 1,
     name: "Pedra Quartzo Roxo",
     price: 100,
-    foto:
+    imgUrl:
       "https://images.pexels.com/photos/4040567/pexels-photo-4040567.jpeg?cs=srgb&dl=pexels-karolina-grabowska-4040567.jpg&fm=jpg",
   },
   {
     id: 2,
     name: "Pedra Mística de Vênus",
     price: 200,
-    foto: "https://images.pexels.com/photos/9037438/pexels-photo-9037438.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+    imgUrl: "https://images.pexels.com/photos/9037438/pexels-photo-9037438.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
   },
   {
     id: 3,
     name: "Pedra Mística de Jovem Místico",
     price: 300,
-    foto:
+    imgUrl:
       "https://images.pexels.com/photos/1573247/pexels-photo-1573247.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
   },
   {
     id: 4,
     name: "Pedra Mística de Cogumelo",
     price: 400,
-    foto:
+    imgUrl:
       "https://i.pinimg.com/originals/bf/96/48/bf9648be9dd4a19f012bfc2b35aa3dd9.jpg",
   },
   
 ];
+
 class App extends React.Component {
   state = {
     carrinho: []
@@ -48,12 +53,11 @@ class App extends React.Component {
     return (
       <div>
         <Produtos
-          quantidadeDeProdutos={products}
-          produtos={products}
-          onClick={this.adicionarProdutoNoCarrinho}
+
+            quantidadeDeProdutos={products}
+            produtos={products}
+            onClick={this.adicionarProdutoNoCarrinho}
         />
-        <CardProdutos/>
-        
       </div>
     );
   }
--- labespace/src/Components/Produtos.jsx
+++ labespace/src/Components/Produtos.jsx
@@ -2,52 +2,53 @@ import React from "react";
 import {CardProdutos} from "./CardProdutos";
 
 
-export default class Produtos extends React.Component {
-    
-    state = {
-        ordenacao: "Decrescente"
-      };
-    
-      ordenarProdutos = (event) => {
-        this.setState({
-          ordenacao: event.target.value
-        });
-      };
 
-      render() {
-        const produtosOrdenadosEmapeados = this.props.produtos
-          .sort((a, b) => {
-            return this.state.ordenacao === "Crescente"
-              ? a.price - b.price
-              : b.price - a.price;
-          })
-          .map((produto) => (
-            <div
-              key={produto.id}
-              id={produto.id}
-              imagemProduto={produto.foto}
-              nome={produto.name}
-              preco={produto.price}
-              onClick={() => this.props.onClick(produto)}
-            />
-          ));
-    
-        return (
-          <div>
-            <p>
-                Quantidade de produtos: {this.props.quantidadeDeProdutos.length}
-              </p>
-              <label>
-                Ordenação:
-                <select onChange={this.ordenarProdutos}>
-                  <option value={"Decrescente"}>Decrescente</option>
-                  <option value={"Crescente"}>Crescente</option>
-                </select>
-              </label>
-               
-            <div>{produtosOrdenadosEmapeados}</div>
-          </div>
-        );
-      }
-    }
-    
+export class Produtos extends React.Component {
+  state = {
+    ordenacao: "Decrescente"
+  };
+
+  ordenarProdutos = (e) => {
+    this.setState({
+      ordenacao: e.target.value
+    });
+  };
+
+  render() {
+    const produtosOrdenados = this.props.produtos
+      .sort((a, b) => {
+        return this.state.ordenacao === "Crescente"
+          ? a.price - b.price
+          : b.price - a.price;
+      })
+      .map((produto) => (
+        <CardProdutos
+          key={produto.id}
+          id={produto.id}
+          imagemProdutos={produto.imgUrl}
+          nome={produto.name}
+          preco={produto.price}
+          onClick={() => this.props.onClick(produto)}
+        />
+      ));
+
+    return (
+      <div>
+        <div>
+          <p>
+            Quantidade de produtos: {this.props.quantidadeDeProdutos.length}
+          </p>
+          <label>
+            Ordenação:
+            <select onChange={this.ordenarProdutos}>
+              <option value={"Decrescente"}>Decrescente</option>
+              <option value={"Crescente"}>Crescente</option>
+            </select>
+          </label>
+        </div>
+
+        <div>{produtosOrdenados}</div>
+      </div>
+    );
+  }
+}
